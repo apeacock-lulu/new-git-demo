f6444eb196139b9bda0cd0766aecaefd34de7fe3
root <a18578096953@163.com>
2022-07-24 19:33:30 +0800

    second commit

--- src/main/java/com/cqq/gitdemo/Service/Impl/PrintServiceImpl.java
+++ src/main/java/com/cqq/gitdemo/Service/Impl/PrintServiceImpl.java
@@ -13,11 +13,11 @@ public class PrintServiceImpl implements PrintService {
         String str = "";
 
         if(i == 1){
-            str = "输出了：1 b的Springboot的okki";
+            str = "输出了：1 bb的Springboot的okki";
         }else if(i == 2){
-            str = "输出了：2 b的Springboot的okki";
+            str = "输出了：2 bb的Springboot的okki";
         }else{
-            str = "输出了：int b的Springboot的okki";
+            str = "输出了：int bb的Springboot的okki";
         }
 
         return str;
