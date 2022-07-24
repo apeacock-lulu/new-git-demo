fb04add05ed202377a93f15fc9cd82e0781ca968
apeacock-lulu <99643058+apeacock-lulu@users.noreply.github.com>
2022-07-24 20:38:04 +0800

    Update PrintServiceImpl.java

--- src/main/java/com/cqq/gitdemo/Service/Impl/PrintServiceImpl.java
+++ src/main/java/com/cqq/gitdemo/Service/Impl/PrintServiceImpl.java
@@ -13,11 +13,11 @@ public class PrintServiceImpl implements PrintService {
         String str = "";
 
         if(i == 1){
-            str = "输出了：1 bbb的Springboot的okki";
+            str = "输出了：1 bbb GitHub 的Springboot的okki";
         }else if(i == 2){
-            str = "输出了：2 aaa的Springboot的okki";
+            str = "输出了：2 aaa GitHub 的Springboot的okki";
         }else{
-            str = "输出了：int aaa的Springboot的okki";
+            str = "输出了：int aaa GitHub 的Springboot的okki";
         }
 
         return str;
